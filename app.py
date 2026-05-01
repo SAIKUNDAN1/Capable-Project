c2a3df5cc02fc9f41af9a267f8a6a7633a8ae516
sreejamanthena10 <sreeja.manthena10@gmail.com>
2026-05-01 13:30:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-
-
-import subprocess
-import sys
-
-# This forces Streamlit to install the missing library if it's not found
-try:
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "langchain", "langchain-community", "langchain-text-splitters"])
-    from langchain.text_splitter import RecursiveCharacterTextSplitterimport streamlit as st
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
